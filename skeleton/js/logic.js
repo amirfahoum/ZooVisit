51b8c1b9916705f844297f1e4d5c5c90c23cec3e
amirfahoum <amirfahoum@gmail.com>
2020-03-31 16:32:07 +0300

    dec functions

--- skeleton/js/logic.js
+++ skeleton/js/logic.js
@@ -5,7 +5,7 @@
 var todoFunctions = {
   // todoFunctions.generateId() will give you a unique id
   // You do not need to understand the implementation of this function.
-  generateId: (function() {
+  generateId: (function () {
     var idCounter = 0;
 
     function incrementCounter() {
@@ -14,33 +14,62 @@ var todoFunctions = {
 
     return incrementCounter;
   })(),
-  
+
   //cloneArrayOfObjects will create a copy of the todos array 
   //changes to the new array don't affect the original
-  cloneArrayOfObjects: function(todos) {
-    return todos.map(function(todo){
+  cloneArrayOfObjects: function (todos) {
+    return todos.map(function (todo) {
       return JSON.parse(JSON.stringify(todo));
     });
   },
-  
-  addTodo: function(todos, newTodo) {
+
+  addTodo: function (todos, newTodo) {
+
+    var newarr = this.cloneArrayOfObjects(todos);
+    let id1 = this.generateId();
+    newObj = { id: id1, description: newTodo, done: false }
+
+    newarr.push(newObj);
+
+    return newarr;
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // returns a new array, it should contain todos with the newTodo added to the end.
     // add an id to the newTodo. You can use the generateId function to create an id.
     // hint: array.concat
   },
-  deleteTodo: function(todos, idToDelete) {
+  deleteTodo: function (todos, idToDelete) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // return a new array, this should not contain any todo with an id of idToDelete
     // hint: array.filter
+
+    var newarr = this.cloneArrayOfObjects(todos);
+    newarr = newarr.filter(function (obj) {
+      return obj.id != idToDelete;
+    });
+    return newarr
   },
-  markTodo: function(todos, idToMark) {
+
+  markTodo: function (todos, idToMark) {
+    var newarr = this.cloneArrayOfObjects(todos);
+    newarr = newarr.map(function (obj) {
+      var newarr = this.cloneArrayOfObjects(todos);
+      newarr = newarr.map(function (obj) {
+        if (obj.id == idToMark) {
+          if (obj.done != true) {
+            obj.done = true;
+          } else obj.done = false;
+        }
+        return obj;
+      })
+      return newarr;
+    })
+
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // in the new todo array, all elements will remain unchanged except the one with id: idToMark
     // this element will have its done value toggled
     // hint: array.map
   },
-  sortTodos: function(todos, sortFunction) {
+  sortTodos: function (todos, sortFunction) {
     // stretch goal! Do this last
     // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
     // sortFunction will have same signature as the sort function in array.sort
